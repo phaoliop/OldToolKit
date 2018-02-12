6b15cf490f60ad87d2c820d20a5f74a194a25449
Paolo Ramirez <phaoliop@gmail.com>
2018-02-12 00:50:15 -0500

    Update ex_prof.bat

--- ex_prof.bat
+++ ex_prof.bat
@@ -1,9 +1,6 @@
 @echo off
 title OldToolKit - Exportar Perfiles
 echo ----------------------------------------
-echo Cambiando de directorio
-for /f "tokens=*" %%A in ('echo %0') do (cd %%~dA
-cd %%~pA)
 echo Creando Directorio
 if not exist \PROFILES-RED mkdir PROFILES-RED
 cd PROFILES-RED
